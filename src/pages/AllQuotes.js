e54cf9974bb9d96a2f2e42f8c6c7eaa58b4dfa5a
Aditya Singh <adityasingh@mobikasa.com>
2022-05-23 14:33:53 +0530

    Newquote revamped

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,13 +1,12 @@
 import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
-    {id: 'q1', author: 'someName', text: 'Some text1'},
-    {id: 'q2', author: 'someName1', text: 'Some text2'},
-    {id: 'q3', author: 'someName2', text: 'Some text3'},
-
-]
+  { id: "q1", author: "someName", text: "Some text1" },
+  { id: "q2", author: "someName1", text: "Some text2" },
+  { id: "q3", author: "someName2", text: "Some text3" },
+];
 
 const AllQuotes = () => {
-    return <QuoteList quotes={DUMMY_QUOTES}/>
-}
-export default AllQuotes
+  return <QuoteList quotes={DUMMY_QUOTES} />;
+};
+export default AllQuotes;
