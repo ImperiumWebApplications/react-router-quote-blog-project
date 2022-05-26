638a85f7bf24c4941de6b25d5969bbafa450f85c
Aditya Singh <adityasingh@mobikasa.com>
2022-05-26 08:51:44 +0530

    Lazy loading incoporated for NewQuote route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import React, { Suspense } from "react";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
 
 function App() {
   return (
-    <Layout>
-      <Switch>
-        <Route path={"/"} exact={true}>
-          <Redirect to={"/quotes"} />
-        </Route>
-        <Route path={"/quotes"} component={AllQuotes} exact={true} />
-        <Route path={"/quotes/:quoteID"} component={QuoteDetail} />
-        <Route path={"/new-quote"} component={NewQuote} />
-        <Route path={"*"} component={NotFound} />
-      </Switch>
-    </Layout>
+    <Suspense
+      fallback={
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <Layout>
+        <Switch>
+          <Route path={"/"} exact={true}>
+            <Redirect to={"/quotes"} />
+          </Route>
+          <Route path={"/quotes"} component={AllQuotes} exact={true} />
+          <Route path={"/quotes/:quoteID"} component={QuoteDetail} />
+          <Route path={"/new-quote"} component={NewQuote} />
+          <Route path={"*"} component={NotFound} />
+        </Switch>
+      </Layout>
+    </Suspense>
   );
 }
 
